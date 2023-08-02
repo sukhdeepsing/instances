e0cd7c62ed4e8e3811ea8dd1c6556faa77449153
sukhdeep singh <sukhdeep@arintech.in>
2023-08-02 07:33:25 +0000

    jh

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ resource "aws_instance" "my_vm" {
   ami = "ami-0c65adc9a5c1b5d7c"
   subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
   instance_type = "t3.micro"
+  key_name = data.terraform_remote_state.network_details.outputs.my_aws_key
   tags = {
      Name = "student.1-vm1"
   }
