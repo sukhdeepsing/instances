afd23ec2554e3ff6ef26357dcbcef461d2e0b506
sukhdeep singh <sukhdeep@arintech.in>
2023-08-02 05:48:41 +0000

    End Of Lab:Lauching VMs

--- main.tf
+++ main.tf
@@ -6,4 +6,12 @@ data "terraform_remote_state" "network_details" {
     region = "us-west-2"
   }
 }
+resource "aws_instance" "my_vm" {
+  ami = "ami-0c65adc9a5c1b5d7c"
+  subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+  instance_type = "t3.micro"
+  tags = {
+     Name = "student.1-vm1"
+  }
+}
 
