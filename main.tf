f14f61684e61375c6694e1a3084509fc1d7b1a0c
sukhdeep singh <sukhdeep@arintech.in>
2023-08-02 09:07:37 +0000

    End of SSH Keys and Security Groups Lab

--- main.tf
+++ main.tf
@@ -11,6 +11,8 @@ resource "aws_instance" "my_vm" {
   subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
   instance_type = "t3.micro"
   key_name = data.terraform_remote_state.network_details.outputs.my_aws_key
+  vpc_security_group_ids =  data.terraform_remote_state.network_details.outputs.security_group_id_array
+
   tags = {
      Name = "student.1-vm1"
   }
