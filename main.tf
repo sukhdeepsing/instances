f65821efc55b149dbf510eb5ff5c92e1f9d4b1c8
sukhdeep singh <sukhdeep@arintech.in>
2023-08-03 06:20:23 +0000

    iu

--- main.tf
+++ main.tf
@@ -16,7 +16,18 @@ module "webserver" {
   subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
   vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
    tags = {
-      " Name" = "student.1-vm1"
+      " Name" = "student.1-webserver-vm"
   }
 }
-
+module "loadbalancer" {
+  source = "./modules/linux_node"
+  ami = "ami-0c65adc9a5c1b5d7c"
+  instance_count = "0"
+  instance_type = "t3.micro"
+  key_name  = data.terraform_remote_state.network_details.outputs.my_aws_key
+  subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+  vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
+   tags = {
+      " Name" = "student.1-loadbalancer-vm"
+  }
+}
--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
 output "public_ip" {
  value = module.webserver.*.public_ip
 }
+output "loadbalancer_vm_public_ip" {
+ value = module.loadbalancer.*.public_ip
+}
