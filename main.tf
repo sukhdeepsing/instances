7ba26938f6e7282a1f597d0a79da66ac2384f807
sukhdeep singh <sukhdeep@arintech.in>
2023-08-03 05:58:00 +0000

    kjh

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ data "terraform_remote_state" "network_details" {
 module "webserver" {
   source = "./modules/linux_node"
   ami = "ami-0c65adc9a5c1b5d7c"
+  instance_count = "1"
   instance_type = "t3.micro" 
   key_name  = data.terraform_remote_state.network_details.outputs.my_aws_key
   subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
--- modules/linux_node/variables.tf
+++ modules/linux_node/variables.tf
@@ -11,3 +11,4 @@ variable "ami" {}
 
 variable "tags" {}
 
+variable "instance_count" {}
